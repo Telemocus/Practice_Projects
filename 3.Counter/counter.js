1302935a6f3f984136b97023fc40bc6a844d88db
Mocus <mocussmncube@gmail.com>
2023-11-26 10:46:18 +0200

    added color to the body using js

--- 3.Counter/counter.js
+++ 3.Counter/counter.js
@@ -2,28 +2,31 @@ const increaseBtn = document.getElementById("increase-control");
 const decreaseBtn = document.getElementById("decrease-control");
 const counterDisplayElement = document.getElementById("counterDisplay");
 
+
 let total = 0;
 
 const limit = 20;
 
-const updateCounterDisplay = ()=>{
-     if(total > limit){
-          total = limit;
-     }    
-     if (total < 0 ) {
-          total = 0;
-     }
-     counterDisplayElement.textContent = total;
-}
+const updateCounterDisplay = () => {
+  if (total > limit) {
+    total = limit;
+  }
+  if (total < 0) {
+    total = 0;
+  }
+  counterDisplayElement.textContent = total;
 
+  document.body.style.setProperty('background-color',`rgb(${(total / limit) * 255 } , 64 , 0 )`)
+};
 
-increaseBtn.addEventListener('click', ()=>{
-     total += 1;
-     updateCounterDisplay();
-})
+increaseBtn.addEventListener("click", () => {
+  total += 1;
+  updateCounterDisplay();
+});
 
-decreaseBtn.addEventListener('click',()=>{
-     total -= 1 ;
-   
-     updateCounterDisplay();
-})
+decreaseBtn.addEventListener("click", () => {
+  total -= 1;
+
+  updateCounterDisplay();
+});
+
